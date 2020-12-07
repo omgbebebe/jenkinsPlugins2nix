90469b5dd6835abc12a835b2a05e45dcf8d12dcf
omg <omgbebebe@gmail.com>
2020-12-07 16:49:10 +0400

    bump haskell-nix version

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8101" }:
 let
   inherit (nixpkgs) pkgs;
   haskellPackages = if compiler == "default"
@@ -8,8 +8,8 @@ let
   hnix = import (nixpkgs.fetchFromGitHub {
             owner = "haskell-nix";
             repo = "hnix";
-            rev = "0.6.1";
-            sha256 = "1v1g63icw66290lsr8g51gb13dgm3xbgqdq71q2g0v0bfc2nfd1c";
+            rev = "0.10.1";
+            sha256 = "1df6944z31q0wh4r6v2gb1fglxsknyd5wh4574n2nn8s0bbnw06m";
             }) { inherit (nixpkgs) pkgs; compiler = compiler; };
 in
 haskellPackages.callPackage jenkinsPlugins2nix { hnix = pkgs.haskell.lib.dontCheck hnix; }
